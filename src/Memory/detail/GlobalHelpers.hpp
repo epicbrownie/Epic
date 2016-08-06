e5e98a8ff43570a4aa9d339c3eb2428f77d339b1
EpicBrownie <epicbrownie@gmail.com>
2016-08-06 12:28:59 -0500

    UnwrapGlobalAllocator

---
+++ src/Memory/detail/GlobalHelpers.hpp
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//            Copyright (c) 2016 Ronnie Brohn (EpicBrownie)      
+//
+//                Distributed under The MIT License (MIT).
+//             (See accompanying file License.txt or copy at 
+//                 https://opensource.org/licenses/MIT)
+//
+//           Please report any bugs, typos, or suggestions to
+//              https://github.com/epicbrownie/Epic/issues
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace Epic
+{
+	template<class Allocator, class Tag>
+	class GlobalAllocatorImpl;
+
+	namespace detail
+	{
+		template<class A> 
+		struct UnwrapGlobalAllocator;
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+template<class A>
+struct Epic::detail::UnwrapGlobalAllocator
+{
+	using type = A;
+};
+
+template<class A, class Tag>
+struct Epic::detail::UnwrapGlobalAllocator<Epic::GlobalAllocatorImpl<A, Tag>>
+{
+	using type = typename UnwrapGlobalAllocator<A>::type;
+};
