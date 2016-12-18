9d3bcd404c054740e450246784f6a6bb47d227ef
EpicBrownie <epicbrownie@gmail.com>
2016-12-17 22:23:41 -0600

    Renamed for expanded tag dispatching usage

---
+++ src/Math/detail/MathHelpers.hpp
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//            Copyright (c) 2016 Ronnie Brohn (EpicBrownie)      
+//
+//                Distributed under The MIT License (MIT).
+//             (See accompanying file License.txt or copy at 
+//                 https://opensource.org/licenses/MIT)
+//
+//           Please report any bugs, typos, or suggestions to
+//              https://github.com/epicbrownie/Epic/issues
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace Epic
+{
+	struct OnesTag { };
+	struct ZeroesTag { };
+	struct IdentityTag { };
+	
+	struct TranslationTag { };
+	struct ScaleTag { };
+
+	struct RotationTag { };
+	struct XRotationTag { };
+	struct YRotationTag { };
+	struct ZRotationTag { };
+
+	struct LookAtTag { };
+	struct FrustumTag { };
+	struct PerspectiveTag { };
+	struct OrthoTag { };
+	struct Ortho2DTag { };
+	struct ShearTag { };
+	struct PickingTag { };
+	struct ShadowTag { };
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace Epic
+{
+	static OnesTag One;
+	static ZeroesTag Zero;
+	static IdentityTag Identity, Origin;
+
+	static TranslationTag Translation;
+	static ScaleTag Scale;
+
+	static RotationTag Rotation;
+	static XRotationTag XRotation;
+	static YRotationTag YRotation;
+	static ZRotationTag ZRotation;
+
+	static LookAtTag LookAt;
+	static FrustumTag Frustum;
+	static PerspectiveTag Perspective;
+	static OrthoTag Ortho;
+	static Ortho2DTag Ortho2D;
+	static ShearTag Shear;
+	static PickingTag Picking;
+	static ShadowTag Shadow;
+}
