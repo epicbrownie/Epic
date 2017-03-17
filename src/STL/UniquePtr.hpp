cb327a60360191c394b993e31a5cc46d12373236
EpicBrownie <epicbrownie@gmail.com>
2017-03-17 18:30:30 -0500

    Epic::ImplPtr now type erases the allocator to allow for container storage even when the concrete types have different allocators.
    For now, ImplPtr and UniquePtr are incompatible with each other.

--- src/STL/UniquePtr.hpp
+++ src/STL/UniquePtr.hpp
@@ -24,6 +24,11 @@ namespace Epic::detail
 {
 	template<class AllocatorType>
 	struct Deleter;
+
+	template<class T, class AllocatorType>
+	struct ImplDeleterFn;
+
+	struct ImplDeleter;
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -32,10 +37,38 @@ namespace Epic
 {
 	template<class T, class A = Epic::DefaultAllocatorFor<T, Epic::eAllocatorFor::UniquePtr>>
 	using UniquePtr = std::unique_ptr<T, Epic::detail::Deleter<A>>;
+
+	template<class T>
+	using ImplPtr = std::unique_ptr<T, Epic::detail::ImplDeleter>;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
+// ImplDeleterFn
+template<class T, class A>
+struct Epic::detail::ImplDeleterFn
+{
+	static void Apply(void* pObj, size_t extent)
+	{
+		using AllocatorType = Epic::STLAllocator<T, A>;
+		using AllocatorTraits = std::allocator_traits<AllocatorType>;
+
+		AllocatorType allocator;
+
+		auto p = reinterpret_cast<T*>(pObj);
+
+		// Destroy pObject(s)
+		for (size_t i = 0; i < extent; ++i)
+			AllocatorTraits::destroy(allocator, &p[i]);
+
+		// Deallocate memory block
+		AllocatorTraits::deallocate(allocator, p, extent);
+	}
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+// Deleter
 template<class A>
 struct Epic::detail::Deleter
 {
@@ -66,8 +99,33 @@ struct Epic::detail::Deleter
 	}
 };
 
+// ImplDeleter
+struct Epic::detail::ImplDeleter
+{
+	using TypeErasedDeleterFn = void(*)(void*, size_t);
+
+	size_t m_Extent;
+	TypeErasedDeleterFn m_pDeleterFn;
+
+	ImplDeleter() noexcept
+		: m_Extent{ 0 }, m_pDeleterFn{ nullptr } { }
+
+	ImplDeleter(const size_t extent, TypeErasedDeleterFn pDelete) noexcept
+		: m_Extent{ extent }, m_pDeleterFn{ pDelete } { }
+
+	template<class T>
+	void operator() (T* p)
+	{
+		assert(m_Extent > 0);
+		assert(m_pDeleterFn);
+
+		m_pDeleterFn(p, m_Extent);
+	}
+};
+
 //////////////////////////////////////////////////////////////////////////////
 
+// MakeUnique
 namespace Epic
 {
 	/// MakeUnique<T, A, Args...>
@@ -96,7 +154,7 @@ namespace Epic
 		}
 
 		// Construct the unique_ptr
-		return std::unique_ptr<T, detail::Deleter<A>>{ pObject, { 1 } };
+		return Epic::UniquePtr<T, A>{ pObject, { 1 } };
 	}
 
 	/// MakeUnique<T[], A>
@@ -131,7 +189,7 @@ namespace Epic
 		}
 
 		// Construct the unique_ptr
-		return std::unique_ptr<Elem[], detail::Deleter<A>>{ pObjects, { Count } };
+		return std::unique_ptr<Elem[], A>{ pObjects, { Count } };
 	}
 
 	/// MakeUnique<T, Types...>
@@ -142,12 +200,13 @@ namespace Epic
 
 //////////////////////////////////////////////////////////////////////////////
 
+// MakeImpl
 namespace Epic
 {
 	/// MakeImpl<B, D, A, Args...>
 	template<class B, class D, class A = Epic::DefaultAllocatorFor<D, Epic::eAllocatorFor::UniquePtr>, class... Args>
 	inline typename std::enable_if<!std::is_array<B>::value, 
-		Epic::UniquePtr<B, A>>::type
+		Epic::ImplPtr<B>>::type
 	MakeImpl(Args&&... args)
 	{
 		using AllocatorType = Epic::STLAllocator<D, A>;
@@ -170,6 +229,6 @@ namespace Epic
 		}
 
 		// Construct the unique_ptr
-		return std::unique_ptr<B, detail::Deleter<A>>{ pObject, { 1 } };
+		return Epic::ImplPtr<B>{ pObject, { 1, &detail::ImplDeleterFn<D, A>::Apply } };
 	}
 }
