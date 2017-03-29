bdec4e7057f059e6e91829c5665dd19e3283308b
EpicBrownie <epicbrownie@gmail.com>
2017-03-29 08:59:08 -0500

    Added SharedPtr<T> alias for consistency with UniquePtr<T>

--- headers/Epic/STL/SharedPtr.hpp
+++ headers/Epic/STL/SharedPtr.hpp
@@ -23,9 +23,17 @@
 
 namespace Epic
 {
+	template<class T>
+	using SharedPtr = std::shared_ptr<T>;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace Epic
+{
 	/// MakeShared<T, A, Args...>
 	template<class T, class A = Epic::DefaultAllocatorFor<T, Epic::eAllocatorFor::SharedPtr>, class... Args>
-	inline std::shared_ptr<T> MakeShared(Args&&... args)
+	inline Epic::SharedPtr<T> MakeShared(Args&&... args)
 	{
 		using AllocatorType = Epic::STLAllocator<T, A>;
 		using AllocatorTraits = std::allocator_traits<AllocatorType>;
@@ -54,13 +62,13 @@ namespace Epic
 			AllocatorTraits::deallocate(alloc, p, 1); 
 		};
 
-		return std::shared_ptr<T>{ pObject, Deleter, allocator };
+		return Epic::SharedPtr<T>{ pObject, Deleter, allocator };
 	}
 
 	/// MakeShared<std::unique_ptr<T, D>>
 	template<class T, class D>
-	inline std::shared_ptr<T> MakeShared(std::unique_ptr<T, D>&& pUnique)
+	inline Epic::SharedPtr<T> MakeShared(std::unique_ptr<T, D>&& pUnique)
 	{
-		return std::shared_ptr<T>{ std::move(pUnique) };
+		return Epic::SharedPtr<T>{ std::move(pUnique) };
 	}
 }
